a959ff542d17f002dfdf7f6f3b561472648bbdf0
romario-developer <romariotecla@icloud.com>
2026-01-11 23:05:18 -0300

    fix: remove invalid index rename from migration

--- prisma/migrations/20260112015946_add_cardpayment_and_installments/migration.sql
+++ prisma/migrations/20260112015946_add_cardpayment_and_installments/migration.sql
@@ -1,8 +1,9 @@
 -- CreateIndex
+
 CREATE INDEX "Expense_userId_date_idx" ON "Expense"("userId", "date");
 
 -- RenameIndex
-ALTER INDEX "CardPayment_user_card_cycle_idx" RENAME TO "CardPayment_userId_cardId_cycleEnd_idx";
+-- ALTER INDEX "CardPayment_user_card_cycle_idx" RENAME TO "CardPayment_userId_cardId_cycleEnd_idx";
 
 -- RenameIndex
 ALTER INDEX "Expense_user_card_date_idx" RENAME TO "Expense_userId_cardId_date_idx";
